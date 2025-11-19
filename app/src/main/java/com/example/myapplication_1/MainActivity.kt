4e9bc9310fd3a382a9b2b18c5514cff7ebf0838c
Sagar Yadav <saga3612u@gmail.com>
2025-11-19 18:59:24 +0530

    done today

--- app/src/main/java/com/example/myapplication_1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication_1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val act2 = findViewById<TextView>(R.id.Forgetbt)
+        act2.setOnClickListener {
+            val Intent = Intent(this, secondactivity::class.java)
+            startActivity(Intent)
         }
+
     }
 }
