8d83ae6fea4be1a4116866270e70fc7831fe5c88
Sagar Yadav <saga3612u@gmail.com>
2025-11-20 13:49:31 +0530

    today

--- app/src/main/java/com/example/myapplication_1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_1/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +17,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val act2 = findViewById<TextView>(R.id.Forgetbt)
+        val login = findViewById< Button>(R.id.button2)
+        val signup = findViewById<TextView>(R.id.signup)
         act2.setOnClickListener {
             val Intent = Intent(this, secondactivity::class.java)
             startActivity(Intent)
+        }
+        login.setOnClickListener {
+            val Go = Intent(this, thirdactivity::class.java)
+            startActivity(Go)
+        }
+        signup.setOnClickListener {
+            val sign = Intent(this, Forthactivity::class.java)
+            startActivity(sign)
         }
 
     }
